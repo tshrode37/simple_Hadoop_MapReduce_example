e4c8949e3a1a72f8fcd21b47d751d6482df6b15a
Taylor Shrode <tshrode37@gmail.com>
2020-01-24 17:39:33 +0000

    lowecase text and remove stopwords and punctuation

--- mapper.py
+++ mapper.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import sys
+import string
+from sklearn.feature_extraction import stop_words
 
+stopwords = set(stop_words.ENGLISH_STOP_WORDS)
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+    punct = line.translate(string.maketrans("",""), string.punctuation)
 
     # split the line into words; splits on any whitespace
-    words = line.split()
 
+    words = punct.split()
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+	if word not in stopwords:
+           print '%s\t%s' % (word, "1")
